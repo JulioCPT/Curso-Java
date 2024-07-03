04cea2fc1d644226b4a22f93d58248b830405a8e
Júlio César Teixeira <130468355+JulioCPT@users.noreply.github.com>
2024-07-03 16:54:11 -0300

    Add files via upload

---
+++ Operadores.java
@@ -0,0 +1,35 @@
+public class Operadores { // declaração da classe Operadores
+
+    public static void main(String[] args) throws Exception { // declaração do método main, que no caso é o método executor
+        System.out.println("Operador de atribuição: =");   // print do operador matemático de atribuicao
+        String texto = "Texto"; // declaração da variável do tipo String, cujo valor recebido é "Texto"
+        System.out.println("String texto = \"Texto\";"); // print com o exemplo de como usar o operador
+        System.out.println(texto); // print com o resultado da variável atribuída
+
+        System.out.println("Operador de soma: +"); // print do operador matemático que é a soma
+        int soma = 1 + 1; // declaração da variável do tipo Inteiro, cujo valor recebido é o resultado da soma de 1 + 1 que é 2.
+        System.out.println("int soma = 1 + 1;"); // print com o operador de soma sendo utilizado com o tipo int mostrando o cálculo que tem que ser feito
+        System.out.println(soma); // print com o resultado do cálculo matemático
+
+        System.out.println("Operador de subtracao: -"); // print do operador matemático que é a subtracao
+        int subtracao = 1 - 1; // declaração da variável do tipo Inteiro, cujo valor recebido é o resultado da subtracao de 1 - 1 que é 0.
+        System.out.println("int subtracao = 1 - 1;"); // print com o operador de subtracao sendo utilizado com o tipo int mostrando o cálculo que tem que ser feito
+        System.out.println(subtracao); // print com o resultado do cálcuo matemático
+
+        System.out.println("Operador de divisao: /"); // print do operador matemático que é a divisao
+        int divisao = 10 / 2; // declaração da variável do tipo Inteiro, cujo valor recebido é o resultado da divisão de 10 / 2 que é 5.
+        System.out.println("int divisao = 10 / 2;"); // print com o operador de divisao sendo utilizado com o tipo int mostrando o cálculo que tem que ser feito
+        System.out.println(divisao); // print com o resultado do cálculo matemático
+
+        System.out.println("Operador de multiplicacao: *"); // print do operador matemático que é a multiplicacao
+        int multiplicacao = 10 * 2; // declaração da variável do tipo Inteiro, cujo valor recebido é o resultado da multiplicação de 10 * 2 que é 20.
+        System.out.println("int multiplicacao = 10 * 2;"); // print com o operador de multiplicacao sendo utilizado com o tipo int mostrando o cálculo que tem que ser feito
+        System.out.println(multiplicacao); // print com o resultado do cálculo matemático
+
+        System.out.println("Operador de concatenacao de strings: +"); // print do operador concatenacao de strings
+        String concatenacao = "Texto 1 +" + "Texto 2"; // declaração da variável do tipo String, cujo valor recebido é "Texto 1 + Texto 2"
+        System.out.println("String concatenacao = \"Texto 1 + \" + \"Texto 2\";"); // print com o operador de concatenacao sendo utilizado com o tipo String
+        System.out.println(concatenacao); // print com o resultado da concatenação feita
+
+    }
+}
