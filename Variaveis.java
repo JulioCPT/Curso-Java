42d4e16e935d6fd32980b08d908e73cd5fe0036d
Júlio César Teixeira <130468355+JulioCPT@users.noreply.github.com>
2024-07-04 15:11:40 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,28 @@
+ public class Variaveis{   
+    public static void main(String[] args) throws Exception{
+        int varInt = 2; // declaração da variável varInt, do tipo int, com valor recebido 2
+        System.out.println("Varivavel do tipo int (inteiro): " + varInt);
+
+        Integer varInteger = 3;  // declaração da variável varInteger, do tipo int, com valor recebido 3
+        System.out.println("Varivavel do tipo integer (inteiro): " + varInteger);
+
+        String varString = "texto"; // declaração da variável varString, do tipo String, com valor recebido "texto"
+        System.out.println("Varivavel do tipo String (texto): " + varString);
+
+        boolean varBoolean = true; // declaração da variável varBoolean, do tipo booleano, com valor recebido true
+        System.out.println("Varivavel do tipo Booleano (boolean): " + varBoolean);
+
+        byte varByte = 1; // declaração da variável varByte, do tipo Byte, com valor recebido 1
+        System.out.println("Varivavel do tipo Byte (inteiro): " + varByte);
+
+        char varChar = 'r'; // declaração da variável varChar, do tipo Char, com valor recebido "r"
+        System.out.println("Varivavel do tipo Char (texto): " + varChar);
+
+
+        float varFloat = 19.99f; // declaração da variável varFloat, do tipo Float, com valor recebido 19.99
+        System.out.println("Varivavel do tipo Float (float): " + varFloat);
+
+        double varDouble = 0.263568954; // declaração da variável varDouble, do tipo Double, com valor recebido 0.263568954
+        System.out.println("Varivavel do tipo Double (double): " + varDouble);
+    }
+}
